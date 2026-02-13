ae333b7efb6ff51c2c06c694f22c5e8a4a944a2d
LeeHyunKyu <lhyun0218@gmail.com>
2026-02-13 16:10:09 +0900

    SCSS to Styled-Component report

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,10 +1,39 @@
+import styled from "styled-components";
+import { flexMixin, fontMixin } from "../style/style";
+import { tag_color, gray } from "../style/style";
+
+// .content-container
+const StyledContent = styled.div`
+  ${flexMixin({ direction: "column", align: "flex-start", gap: 5 })}
+  img {
+    width: 300px;
+    border-radius: 10px;
+    margin-bottom: 3px;
+    min-height: 225px;
+  }
+  span {
+    ${fontMixin({ size: 12 })}
+    color: ${tag_color};
+    border: 1px solid ${tag_color};
+    padding: 4px 5px;
+    border-radius: 3px;
+  }
+  div {
+    ${fontMixin({ size: 18, weight: 600 })}
+  }
+  p {
+    ${fontMixin({ size: 12 })}
+    color: ${gray};
+  }
+`;
+
 export default function Content({ content }) {
   return (
-    <div className="content-container">
+    <StyledContent>
       <img src={content.img} alt={content.title} />
       <span>모집중</span>
       <div>{content.title}</div>
       <p>{content.subtitle}</p>
-    </div>
+    </StyledContent>
   );
 }
